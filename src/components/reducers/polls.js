2b9409c70b9be810e348a059e6ccb34180ef7315
Marcy Silverman <marcysilve225@gmail.com>
2019-03-19 16:55:55 -0700

    added reducer and actions for polls

---
+++ src/components/reducers/polls.js
@@ -0,0 +1,25 @@
+import { CREATE_POLL, CREATE_POLL_PENDING, CREATE_POLL_ERROR } from '../actions/polls';
+
+const initialState = {
+  loading: false,
+  error: null,
+  polls: [] 
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case CREATE_POLL:
+      return { ...state,
+        list: [...state.list, action.payload],
+        loading: false
+      };
+    case CREATE_POLL_PENDING:
+      return { ...state, loading: true };
+    case CREATE_POLL_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+  }
+}
