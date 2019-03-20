a8c5c8da056134aa8e9a3cb2519daa10af1b54cb
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-20 15:08:11 -0700

    refactored

--- src/components/Polls/CreatePoll.js
+++ src/components/Polls/CreatePoll.js
@@ -1,35 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class CreatePoll extends React.PureComponent {
-  static propTypes =  {
-    question: PropTypes.string.isRequired,
-    option: PropTypes.string. isRequired,
-    options: PropTypes.array.isRequired,
-    onChange: PropTypes.func.isRequired,
-    onAdd: PropTypes.func.isRequired,
-    onPost: PropTypes.func.isRequired,
-    onSubmit: PropTypes.func.isRequired,
-  };
-
-  componentDidMount() {
-    console.log(this.props);
-  }
-
-  render() {
-    const { question, option, options, onChange, onAdd, onPost, onSubmit } = this.props;
-    return (
-      <>
-        <h2> Create a Poll</h2>
-        <form onSubmit={onPost}>
-          <input type="text" value={question} name="question" onChange={onChange}/>
-          <input type="text" value={option} name="option" onChange={onChange}/>
-          <button type="button" onClick={onAdd.bind(null, option)}>Add</button>
-          <button type="submit" onClick={onSubmit.bind(null, question, options)}>Submit</button>
-        </form>
-      </>
-    );
-  }
-} 
+function CreatePoll({ question, option, options, onChange, onAdd, onPost, onSubmit }) {
+  return (
+    <>
+      <h2> Create a Poll</h2>
+      <form onSubmit={onPost}>
+        <input type="text" value={question} name="question" onChange={onChange} />
+        <input type="text" value={option} name="option" onChange={onChange} />
+        <button type="button" onClick={onAdd.bind(null, option)}>Add</button>
+        <button type="submit" onClick={onSubmit.bind(null, question, options)}>Submit</button>
+      </form>
+    </>
+  );
+}
+CreatePoll.propTypes = {
+  question: PropTypes.string.isRequired,
+  option: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onAdd: PropTypes.func.isRequired,
+  onPost: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default CreatePoll;
--- src/containers/PollFormContainer.js
+++ src/containers/PollFormContainer.js
@@ -1,7 +1,33 @@
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { getQuestion, getOption, getOptions } from '../selectors/polls';
 import CreatePoll from '../components/polls/CreatePoll';
 import { updateQuestion, updateOption, createPoll, addOption } from '../actions/polls';
+import PropTypes from 'prop-types';
+
+class CreatePollContainer extends PureComponent {
+  static propTypes = {
+    question: PropTypes.string.isRequired,
+    option: PropTypes.string.isRequired,
+    options: PropTypes.array.isRequired,
+    onChange: PropTypes.func.isRequired,
+    onAdd: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired
+  };
+  render() {
+    const { question, option, options, onChange, onAdd, onSubmit } = this.props;
+    return (
+      <CreatePoll
+        question={question}
+        options={options}
+        option={option}
+        onChange={onChange}
+        onAdd={onAdd}
+        onSubmit={onSubmit}
+      />
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   question: getQuestion(state),
@@ -16,7 +42,7 @@ const mapDispatchToProps = (dispatch) => ({
     // console.log(event);
     dispatch(addOption(option));
   },
-  
+
   onChange({ target }) {
     const factoryMethod = {
       question: updateQuestion,
@@ -25,9 +51,9 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch(factoryMethod[target.name](target.value));
   },
 
-  onPost(event) {
-    event.preventDefault();
-  },
+  // onPost(event) {
+  //   event.preventDefault();
+  // },
 
   onSubmit(question, options, event) {
     event.preventDefault();
@@ -38,4 +64,4 @@ const mapDispatchToProps = (dispatch) => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(CreatePoll);
+)(CreatePollContainer);
